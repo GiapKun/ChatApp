d14c02be8e31c57b0b50f4a952cafccc8fd2c27d
GiapKun <trandinhgiap8051@gmail.com>
2024-02-09 23:40:45 +0700

    create signin and signup

---
+++ app/src/main/java/com/example/chatandcall_app/activities/SignInActivity.java
@@ -0,0 +1,26 @@
+package com.example.chatandcall_app.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.example.chatandcall_app.R;
+import com.example.chatandcall_app.databinding.ActivitySignInBinding;
+
+public class SignInActivity extends AppCompatActivity {
+
+    private ActivitySignInBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding= ActivitySignInBinding.inflate(getLayoutInflater());
+        setContentView(R.layout.activity_sign_in);
+        setContentView(binding.getRoot());
+        setListeners();
+    }
+    private  void setListeners(){
+        binding.textCreateNewAccount.setOnClickListener(v->
+                startActivity(new Intent(getApplicationContext(), SignUpActivity.class)));
+    }
+}
