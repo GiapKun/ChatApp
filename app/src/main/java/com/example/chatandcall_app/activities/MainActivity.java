afe24bb5dfa57f9b29f4ea3f5408348c8c8b5645
GiapKun <trandinhgiap8051@gmail.com>
2024-02-12 23:17:36 +0700

    add icon in SignUp

--- app/src/main/java/com/example/chatandcall_app/activities/MainActivity.java
+++ app/src/main/java/com/example/chatandcall_app/activities/MainActivity.java
@@ -1,16 +1,32 @@
 package com.example.chatandcall_app.activities;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.example.chatandcall_app.R;
+import com.example.chatandcall_app.databinding.ActivityMainBinding;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Enabled viewBinding, the binding class for each XML layout will be generated auto
+    private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //đối tượng binding sẽ giữ thông tin về tất cả các thành phần UI
+        binding= ActivityMainBinding.inflate(getLayoutInflater());
+        //setContentView sẽ hiển thị chúng trên màn hình
+        setContentView(binding.getRoot());
+
     }
+
 }
